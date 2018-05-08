75e5d8a9bd59e98ef3df1f0e7e487214e04a179a
SFTLCRPC039\ciprian.sulu <cipriansulu1@yahoo.com>
2018-05-08 11:11:40 +0300

    fixed criteria

--- EvaluationApp.Domain/Criteria.cs
+++ EvaluationApp.Domain/Criteria.cs
@@ -9,6 +9,7 @@ namespace EvaluationApp.Domain
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public DateTime CreatedDate { get; set; }
         public DateTime ModifiedDate { get; set; }
 
         public int CreatedBy { get; set; }
