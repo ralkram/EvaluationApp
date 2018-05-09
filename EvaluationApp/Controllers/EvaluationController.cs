3b34911a27801d0cf38b9d54f22ba018cbb9a7c1
Razvan Popescu <razvanncr@yahoo.com>
2018-05-09 14:16:44 +0300

    Updated controllers and views

---
+++ EvaluationApp/Controllers/EvaluationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EvaluationApp.Core.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EvaluationApp.Controllers
+{
+    public class EvaluationController : Controller
+    {
+        private readonly IEvaluationFormService evaluationFormService;
+
+        public EvaluationController(IEvaluationFormService evaluationFormService)
+        {
+            this.evaluationFormService = evaluationFormService;
+        }
+    }
+}
