9a45ec88453236b823519a9ee6d5f1b61a7b368e
Razvan Popescu <razvanncr@yahoo.com>
2018-05-11 12:42:01 +0300

    Added methods in Evaluation Controller

--- EvaluationApp/Controllers/EvaluationsController.cs
+++ EvaluationApp/Controllers/EvaluationsController.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EvaluationApp.Core.Shared;
+using EvaluationApp.Domain;
+using EvaluationApp.Models;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace EvaluationApp.Controllers
 {
@@ -14,17 +14,61 @@ namespace EvaluationApp.Controllers
         private readonly IEvaluationFormsService evaluationFormsService;
         private readonly IEvaluationsService evaluationsService;
         private readonly IAuthenticationService authenticationService;
+        private readonly IEmployeesService employeesService;
 
         public EvaluationsController(
             IEvaluationFormsService evaluationFormsService,
             IEvaluationsService evaluationsService,
-            IAuthenticationService authenticationService)
+            IAuthenticationService authenticationService, 
+            IEmployeesService employeesService)
         {
             this.evaluationFormsService = evaluationFormsService;
             this.evaluationsService = evaluationsService;
             this.authenticationService = authenticationService;
+            this.employeesService = employeesService;
         }
 
+        public IActionResult InProgress()
+        {
+            int loggedEmployeeId = authenticationService.GetCurrentUserId();
+            var inProgressEvaluations = evaluationsService.GetInProgressEvaluations(loggedEmployeeId);
+            var evaluationViewModels = GenerateEvaluationViewModels(inProgressEvaluations);
 
+            return View(evaluationViewModels);
+        }
+        public IActionResult Completed()
+        {
+            int loggedEmployeeId = authenticationService.GetCurrentUserId();
+            var completedEvaluations = evaluationsService.GetCompletedEvaluations(loggedEmployeeId);
+            var evaluationViewModels = GenerateEvaluationViewModels(completedEvaluations);
+
+            return View(evaluationViewModels);
+        }
+
+        private EvaluationViewModel GenerateEvaluationViewModel(Evaluation evaluation)
+        {
+            EvaluationViewModel evaluationViewModel = new EvaluationViewModel
+            {
+                EvaluationName = evaluation.EvaluationName,
+                FormName = evaluation.FormName,
+                IsCompleted = evaluation.IsCompleted,
+                Sections = evaluation.Sections,
+                Employee = employeesService.GetEmployeeInfo(evaluation.EmployeeId),
+                LastEvaluator = employeesService.GetEmployeeInfo(evaluation.LastEvaluatorId)
+            };
+
+            return evaluationViewModel;
+        }
+
+        private ICollection<EvaluationViewModel> GenerateEvaluationViewModels(ICollection<Evaluation> evaluations)
+        {
+            ICollection<EvaluationViewModel> evaluationViewModels = new List<EvaluationViewModel>();
+
+            foreach (var evaluation in evaluations)
+            {
+                evaluationViewModels.Add(GenerateEvaluationViewModel(evaluation));
+            }
+            return evaluationViewModels;
+        }
     }
 }
