01fa936f21a0768946aca391585c2668c70194ab
Razvan Popescu <razvanncr@yahoo.com>
2018-05-16 16:06:03 +0300

    Removed GradeId from Criteria and added a migration

---
+++ EvaluationApp.Persistence.EF/Migrations/20180516130412_removed_gradeID_from_criteria.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EvaluationApp.Persistence.EF.Migrations
+{
+    public partial class removed_gradeID_from_criteria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
