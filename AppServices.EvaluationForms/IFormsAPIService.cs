bc14c0c4964bc0d77cb1583641880513a9a3872a
Ciprian Sulu <cipriansulu1@yahoo.com>
2018-05-27 02:12:50 +0300

    Added forms api service

---
+++ AppServices.EvaluationForms/IFormsAPIService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace AppServices.EvaluationForms
+{
+    public interface IFormsAPIService
+    {
+        HttpClient Initialize();
+    }
+}
---
+++ Infrastructure.EvaluationFormsService/FormsAPIService.cs
@@ -0,0 +1,18 @@
+﻿using AppServices.EvaluationForms;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Infrastructure.EvaluationFormsService
+{
+    public class FormsAPIService : IFormsAPIService
+    {
+        public HttpClient Initialize()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:58549/");
+            return client;
+        }
+    }
+}
